d77fcba7bc660bdd93c039548751aa580e155780
“Gary <elgafo.nakama@gmail.com>
2022-06-26 22:19:39 -0400

    ADD: Complete side right

--- src/components/SideBarRight.jsx
+++ src/components/SideBarRight.jsx
@@ -1,7 +1,62 @@
+import Lupa from "/lupa.svg";
+import Dots from "/dots.svg";
+
 const SideBarRight = () => {
     return ( 
         <div className="sidebar-right">
             <h3>Sidebar right</h3>
+            <div className="input">
+                <img src={Lupa} alt="" />
+                <input type="text" placeholder="Search Twitter" />
+            </div>
+            <div className="trends">
+                <h3>Trends for you</h3>
+                <div className="row-trend">
+                    <div className="content">
+                        <p>Music . Trending</p>
+                        <h4>#CharliePuth</h4>
+                        <span>209K Tweets</span>
+                    </div>
+                    <a href="#"><img src={Dots} alt="" /></a>
+                </div>
+
+                <div className="row-trend">
+                    <div className="content">
+                        <p>Music . Trending</p>
+                        <h4>#CharliePuth</h4>
+                        <span>209K Tweets</span>
+                    </div>
+                    <a href="#"><img src={Dots} alt="" /></a>
+                </div>
+
+                <div className="row-trend">
+                    <div className="content">
+                        <p>Music . Trending</p>
+                        <h4>#CharliePuth</h4>
+                        <span>209K Tweets</span>
+                    </div>
+                    <a href="#"><img src={Dots} alt="" /></a>
+                </div>
+
+                <div className="row-trend">
+                    <div className="content">
+                        <p>Music . Trending</p>
+                        <h4>#CharliePuth</h4>
+                        {/* <span>209K Tweets</span> */}
+                    </div>
+                    <a href="#"><img src={Dots} alt="" /></a>
+                </div>
+
+                <div className="row-trend">
+                    <div className="content">
+                        <p>Music . Trending</p>
+                        <h4>#CharliePuth</h4>
+                        <span>209K Tweets</span>
+                    </div>
+                    <a href="#"><img src={Dots} alt="" /></a>
+                </div>
+                <a href="#">Show more</a>
+            </div>
         </div>
      );
 }
